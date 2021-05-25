e9a50072b78cdfec97c04911e7e5d6390dbe31cc
JUANK <juank9225@gmail.com>
2021-05-24 22:22:17 -0500

    se crea el proyecto de ping pon de javaScript, se agregan las clases para contruir la pizarra

---
+++ main.js
@@ -0,0 +1,42 @@
+
+(function(){
+
+   self.Board = function(width,height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+
+    }
+
+    self.Board.prototype = {
+
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+            return elements;
+        }
+    }
+})();
+
+(function(){
+    self.BoardView = function(canvas,board){
+        this.canvas=canvas;
+        this.canvas.width = board.width;
+        this.canvas.height=board.height;
+        this.board = board;
+        this.cxt = canvas.getContext("2d");
+
+    }
+})();
+
+self.addEventListener("load",main);
+
+function main(){
+    var board = new Board(800,400);
+    var canvas = document.getElementById('canvas');
+    var boarview = new BoardView(canvas,board);
+
+}
